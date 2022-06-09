e0513fca4e50f7161fd2db41a3256ff90354c1db
bugrasahinnn <sahinbugra97@gmail.com>
2022-06-09 19:49:40 +0300

    Patika Chinese Zodiac

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Dogum Yilinizi Giriniz: ");
+        int yil = inp.nextInt();
+        int kalan = yil % 12;
+
+        switch (kalan) {
+            case 0:
+                System.out.println("Cin Zodyagi Burcunuz : MAYMUN.");
+                break;
+            case 1:
+                System.out.println("Cin Zodyagi Burcunuz : HOROZ.");
+                break;
+            case 2:
+                System.out.println("Cin Zodyagi Burcunuz : KOPEK.");
+                break;
+            case 3:
+                System.out.println("Cin Zodyagi Burcunuz : HOROZ.");
+                break;
+            case 4:
+                System.out.println("Cin Zodyagi Burcunuz : FARE.");
+                break;
+            case 5:
+                System.out.println("Cin Zodyagi Burcunuz : OKUZ.");
+                break;
+            case 6:
+                System.out.println("Cin Zodyagi Burcunuz : KAPLAN.");
+                break;
+            case 7:
+                System.out.println("Cin Zodyagi Burcunuz : TAVSAN.");
+                break;
+            case 8:
+                System.out.println("Cin Zodyagi Burcunuz : EJDERHA.");
+                break;
+            case 9:
+                System.out.println("Cin Zodyagi Burcunuz : YILAN.");
+                break;
+            case 10:
+                System.out.println("Cin Zodyagi Burcunuz : AT.");
+                break;
+            default:
+                break;
+        }
+    }
+}
